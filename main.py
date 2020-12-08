7a5bd1b9d6cb2fd4b2c8075e96c41038cd5383af
Ahmed Mohamed Vall <44106895+ah2@users.noreply.github.com>
2020-12-08 09:38:12 +0400

    removed global variables in

--- main.py
+++ main.py
@@ -2,40 +2,37 @@
 import random
 
 SIZE = 7
-BOARD = []
-TRAP_BOARD = []
-TRAP_NUM = 0
 
 
-def replace(s, i, ch):
-    return s[:i] + ch + s[i + 1:]
+def replace(string, i, ch):
+    return string[:i] + ch + string[i + 1:]
 
 
 def create_board():
-    global BOARD
-    BOARD = ['-' * SIZE] * SIZE
-    BOARD[0] = replace(BOARD[0], random.randrange(SIZE), 'G')
-    BOARD[1] = 'p' * SIZE
+    board = ['-' * SIZE] * SIZE
+    board[0] = replace(board[0], random.randrange(SIZE), 'G')
+    board[1] = 'p' * SIZE
     r = random.sample(range(SIZE), 4)
-    BOARD[SIZE - 1] = replace(BOARD[SIZE - 1], r[0], 'B')
-    BOARD[SIZE - 1] = replace(BOARD[SIZE - 1], r[1], 'B')
-    BOARD[SIZE - 1] = replace(BOARD[SIZE - 1], r[2], 'R')
-    BOARD[SIZE - 1] = replace(BOARD[SIZE - 1], r[3], 'R')
+    board[SIZE - 1] = replace(board[SIZE - 1], r[0], 'B')
+    board[SIZE - 1] = replace(board[SIZE - 1], r[1], 'B')
+    board[SIZE - 1] = replace(board[SIZE - 1], r[2], 'R')
+    board[SIZE - 1] = replace(board[SIZE - 1], r[3], 'R')
+    return board
 
 
 def set_traps():
-    global TRAP_NUM
-    global TRAP_BOARD
-    TRAP_BOARD = [' ' * SIZE] * SIZE
+    trap_num = 0
+    trap_board = [' ' * SIZE] * SIZE
     for j in range(2, SIZE-1):
         for i in range(SIZE):
             if random.random() <= 0.2:
-                TRAP_BOARD[j] = replace(TRAP_BOARD[j], i, 'T')
-                TRAP_NUM += 1
+                trap_board[j] = replace(trap_board[j], i, 'T')
+                trap_num += 1
+    print('This board has {} hidden traps.'.format(trap_num))
+    return trap_board
 
 
-def dis_traps():
-    global TRAP_NUM
+def dis_traps(trap_board):
     lines = ['']*(SIZE+4)
     lines[0] = '  '
     for x in range(SIZE):
@@ -44,7 +41,7 @@ def dis_traps():
     lines[0] += '  '
     lines[1] = " +" + "=" * SIZE + "+ "
     for x in range(SIZE):
-        lines[x+2] = "{}|".format(x) + TRAP_BOARD[x] + "|{}".format(x)
+        lines[x+2] = "{}|".format(x) + trap_board[x] + "|{}".format(x)
     lines[SIZE+2] = " +" + "=" * SIZE + "+ "
     lines[SIZE+3] = lines[0]
 
@@ -52,16 +49,20 @@ def dis_traps():
         print(" ".join(line))
 
 
-def display_board():
+def display_board(board):
     lines = ['']*(SIZE+4)
     lines[0] = '  '
+
     for x in range(SIZE):
         lines[0] += str(x)
         lines[SIZE + 3] += str(x)
+
     lines[0] += '  '
     lines[1] = " +" + "=" * SIZE + "+ "
+
     for x in range(SIZE):
-        lines[x+2] = "{}|".format(x) + BOARD[x] + "|{}".format(x)
+        lines[x+2] = "{}|".format(x) + board[x] + "|{}".format(x)
+
     lines[SIZE+2] = " +" + "=" * SIZE + "+ "
     lines[SIZE+3] = lines[0]
 
@@ -69,16 +70,19 @@ def display_board():
         print(" ".join(line))
 
 
-def validate_moves():
+def validate_moves(r, c, nr, nc, board):
     return True
 
 
-def check_traps():
-    return -1, -1
+def check_traps(r, c, nr, nc, tBoard):
+    return False
 
 
-def move_general():
-    pass
+def move_general(loc, board):
+    for x in range(SIZE):
+        if board[0][x] == 'G':
+            board[0] = replace(board[0], x, ' ')
+            board[0] = replace(board[0], loc, 'G')
 
 
 def move_soldier():
@@ -86,11 +90,10 @@ def move_soldier():
 
 
 def main():
-    create_board()
-    display_board()
-    set_traps()
-    dis_traps()
-    print('This board has {} hidden traps.'.format(TRAP_NUM))
+    board = create_board()
+    trap_board = set_traps()
+    display_board(board)
+    dis_traps(trap_board)
 
 
 if __name__ == '__main__':
